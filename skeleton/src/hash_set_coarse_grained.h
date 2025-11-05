81072cf17de12a59199e64cd205d71e865afa55e
hc2723 <hc2723@ic.ac.uk>
2025-11-05 22:56:00 +0000

    coarse_grained finished

--- skeleton/src/hash_set_coarse_grained.h
+++ skeleton/src/hash_set_coarse_grained.h
@@ -2,33 +2,104 @@
 #define HASH_SET_COARSE_GRAINED_H
 
 #include <cassert>
+#include <algorithm>   // std::find
+#include <cstddef>     // size_t
+#include <functional>  // std::hash
+#include <mutex>       // std::mutex, std::scoped_lock
+#include <utility>     // std::move
+#include <vector>      // std::vector
+
 
 #include "src/hash_set_base.h"
+
+
 
 template <typename T>
 class HashSetCoarseGrained : public HashSetBase<T> {
  public:
-  explicit HashSetCoarseGrained(size_t /*initial_capacity*/) {}
+  explicit HashSetCoarseGrained(size_t initial_capacity)
+      : buckets_(std::max<size_t>(NormalizeCapacity(initial_capacity), kMinBuckets)),
+        size_(0) {}
 
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Add(T elem) final {
+    std::scoped_lock lock(mutex_);
+    size_t i = Index(elem);
+    auto & b = buckets_[i];
+    if(std::find(b.begin(), b.end(), elem) != b.end()) {
+      return false;
+    }
+    b.push_back(std::move(elem));
+    ++size_;
+    if (LoadFactor() > 4.0) {
+      Resize(buckets_.size() * 2);
+    }
+    return true;
   }
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Remove(T elem) final {
+    std::scoped_lock lock(mutex_);
+    size_t i = Index(elem);
+    auto & b = buckets_[i];
+    auto it = std::find(b.begin(), b.end(), elem);
+    if (it == b.end()) {
+      return false;
+    }
+    b.erase(it);
+    --size_;
+    if (LoadFactor() < 1.0) {
+      Resize(buckets_.size() / 2);
+    }
+    return true;
   }
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  [[nodiscard]] bool Contains(T elem) final {
+    std::scoped_lock lock(mutex_);
+    size_t i = Index(elem);
+    auto & b = buckets_[i];
+    return std::find(b.begin(), b.end(), elem) != b.end();
   }
 
   [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
+    std::scoped_lock lock(mutex_);
+    return size_;
   }
+ private:
+  
+  mutable std::mutex mutex_;
+  std::vector<std::vector<T>> buckets_;
+  size_t size_;
+  std::hash<T> hasher_;
+
+  static constexpr size_t kMinBuckets = 4;
+  static constexpr double kMaxLoadFactor = 4.0;
+
+  static size_t NormalizeCapacity(size_t cap) {
+    return cap == 0 ? kMinBuckets : cap;
+  }
+
+  size_t Index(const T& elem) const {
+    return hasher_(elem) % buckets_.size();
+  }
+
+  double LoadFactor() const {
+    return static_cast<double>(size_) / static_cast<double>(buckets_.size());
+  }
+
+
+  void Resize(size_t new_capacity) {
+    std::scoped_lock lock(mutex_);
+    std::vector<std::vector<T>> new_buckets(new_capacity);
+    for (auto& bucket : buckets_) {
+      for (auto& v : bucket) {
+        size_t i = hasher_(v) % new_capacity;
+        new_buckets[i].push_back(std::move(v));
+      }
+    }
+    buckets_.swap(new_buckets);
+
+  }
+
+
 };
 
 #endif  // HASH_SET_COARSE_GRAINED_H
