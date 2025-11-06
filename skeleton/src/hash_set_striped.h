cf637e91fe893bd6b15a5b284364d629c823eefb
hc2723 <hc2723@ic.ac.uk>
2025-11-06 00:12:53 +0000

    hash_set_strped finished

--- skeleton/src/hash_set_striped.h
+++ skeleton/src/hash_set_striped.h
@@ -2,33 +2,124 @@
 #define HASH_SET_STRIPED_H
 
 #include <cassert>
+#include <algorithm>   // std::find
+#include <cstddef>     // size_t
+#include <functional>  // std::hash
+#include <mutex>       // std::mutex, std::scoped_lock
+#include <utility>     // std::move
+#include <vector>      // std::vector
+#include <atomic>
 
 #include "src/hash_set_base.h"
+
+
 
 template <typename T>
 class HashSetStriped : public HashSetBase<T> {
  public:
-  explicit HashSetStriped(size_t /*initial_capacity*/) {}
+  explicit HashSetStriped(size_t initial_capacity, size_t stripes = 64)
+      : buckets_(std::max<size_t>(NormalizeCapacity(initial_capacity), kMinBuckets)),
+        size_(0),
+        locks_(stripes) {}
 
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  
+
+  bool Add(T elem) final {
+    size_t i = Index(elem);
+    size_t stripe = StripeOfBucket(i);
+    {
+      std::unique_lock<std::mutex> lk(locks_[stripe]);
+      auto & b = buckets_[i];
+      if(std::find(b.begin(), b.end(), elem) != b.end()) {
+        return false;
+      }
+      b.push_back(std::move(elem));
+      size_.fetch_add(1, std::memory_order_relaxed);
+    }
+    if (LoadFactor() > 4.0) {
+      Resize(buckets_.size() * 2);
+    }
+    return true;
   }
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Remove(T elem) final {
+    size_t i = Index(elem);
+    size_t stripe = StripeOfBucket(i);
+    {
+      std::unique_lock<std::mutex> lk(locks_[stripe]);
+      auto & b = buckets_[i];
+      auto it = std::find(b.begin(), b.end(), elem);
+      if (it == b.end()) {
+        return false;
+      }
+      b.erase(it);
+      size_.fetch_sub(1, std::memory_order_relaxed);
+    }
+    if (LoadFactor() < 1.0) {
+      Resize(buckets_.size() / 2);
+    }
+    return true;
   }
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  [[nodiscard]] bool Contains(T elem) final {
+    size_t i = Index(elem);
+    size_t stripe = StripeOfBucket(i);
+    {
+      std::unique_lock<std::mutex> lk(locks_[stripe]);
+      auto & b = buckets_[i];
+      return std::find(b.begin(), b.end(), elem) != b.end();
+    }
   }
 
   [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
+    return size_.load(std::memory_order_relaxed);
   }
+ private:
+
+  std::vector<std::vector<T>> buckets_;
+  std::atomic<size_t> size_; 
+  std::hash<T> hasher_;
+  std::vector<std::mutex> locks_;
+
+  static constexpr size_t kMinBuckets = 4;
+  static constexpr double kMaxLoadFactor = 4.0;
+
+  static size_t NormalizeCapacity(size_t cap) {
+    return cap == 0 ? kMinBuckets : cap;
+  }
+
+  size_t Index(const T& elem) const {
+    return hasher_(elem) % buckets_.size();
+  }
+
+  size_t StripeOfBucket(size_t b) const { 
+    return b % locks_.size(); 
+  }
+
+
+  double LoadFactor() const {
+    return static_cast<double>(size_.load(std::memory_order_relaxed)) / static_cast<double>(buckets_.size());
+  }
+
+
+  void Resize(size_t new_capacity) {
+    for (auto& lock : locks_) {
+      lock.lock();
+    }
+    std::vector<std::vector<T>> new_buckets(new_capacity);
+    for (auto& bucket : buckets_) {
+      for (auto& v : bucket) {
+        size_t i = hasher_(v) % new_capacity;
+        new_buckets[i].push_back(std::move(v));
+      }
+    }
+    buckets_.swap(new_buckets);
+    for (auto& lock : locks_) {
+      lock.unlock();
+    }
+  }
+
+
 };
 
-#endif  // HASH_SET_STRIPED_H
+#endif  // HASH_SET_COARSE_GRAINED_H
